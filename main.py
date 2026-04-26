d1f5a399d4e6354219481a413bb2fbc93d6ac4d2
Has <hascass1503@gmail.com>
2026-04-25 19:50:12 -0500

    feat: se crea el main.py para ejecutar los datos

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-# 1. Cambia 'generar_gastos' por 'generar_gasto' (quítale la s)
+# 1. Importar tal cual la estructura de carpetas
 from utils.simulacion_gasto import generar_gasto
+from utils.simulacion_usuario import generar_ususario
 
-# 2. Úsala también en singular
-gastos = generar_gasto(100)
+# 2. Generar la data de usuarios (lo que Lau hizo con probabilidades)
+# Usamos 100 para que el DataFrame tenga buen volumen
+usuarios = generar_ususario(100)
 
-# 3. Convierte a DataFrame y muestra
-gastos_ordenados = pd.DataFrame(gastos)
-print(gastos_ordenados)
+# 3. Convertir a DataFrame (Estructura de la imagen)
+usuarios_ordenados = pd.DataFrame(usuarios)
+
+# 4. Mostrar el resultado
+print(usuarios_ordenados)
