3152afceba1525e2aa2c72785c3c2031c32bcfc1
estefany <hilarygomezh2010@gmail.com>
2026-04-28 18:08:48 -0500

    fix: se arregla el main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 
-#from notebooks.limpiezaUsuario import limpia_simulacion
-#from utils.simulacion_usuario import generar_ususario
-#usuarios = generar_ususario(100)
-#usuarios_ordenados = pd.DataFrame(usuarios)
-#usuarios_limpio = limpia_simulacion(usuarios_ordenados)
-#print(usuarios_limpio)
+from notebooks.limpiezaUsuario import limpia_simulacion
+from utils.simulacion_usuario import generar_ususario
+usuarios = generar_ususario(100)
+usuarios_ordenados = pd.DataFrame(usuarios)
+usuarios_limpio = limpia_simulacion(usuarios_ordenados)
+print(usuarios_limpio)
 
 
 from utils.simulacion_gasto import generar_gasto
